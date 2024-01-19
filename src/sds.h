c826b12c64d4502cf2d3b201abd5b38d21aff7ee
LewX <41771377+LewX@users.noreply.github.com>
2024-01-19 22:51:53 +0800

    Update sds.h

--- src/sds.h
+++ src/sds.h
@@ -67,7 +67,7 @@ struct __attribute__ ((__packed__)) sdshdr32 {
     char buf[];
 };
 struct __attribute__ ((__packed__)) sdshdr64 {
-    uint64_t len; /* used */
+    uint64_t len; /* 表示SDS的长度，buf末尾的空字符 */
     uint64_t alloc; /* excluding the header and null terminator */
     unsigned char flags; /* 3 lsb of type, 5 unused bits */
     char buf[];
